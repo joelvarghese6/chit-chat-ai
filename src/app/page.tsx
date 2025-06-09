3734a28d43f04d3302aafb82cb298fb6332297ed
Joel <varghesejoel6@gmail.com>
2025-06-09 17:28:23 +0530

    added sign-in and sign-up pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
 
 
 export default function Home() {
+
+  const router = useRouter()
+
+  const clickHandler = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/sign-in"); // redirect to login page
+        },
+      },
+    });
+  }
+
   return (
-    <div>
-      Hello World
-    </div>
+    <Button onClick={clickHandler}>Sign out</Button>
   );
 }
