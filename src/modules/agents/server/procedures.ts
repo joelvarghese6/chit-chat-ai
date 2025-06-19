486f7cc5f65894008a7f63f0fb706143f95026d4
Joel <varghesejoel6@gmail.com>
2025-06-19 23:22:39 +0530

    ccreated agents data table and empty page

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -4,12 +4,15 @@ import { agents } from "@/db/schema"
 import { TRPCError } from "@trpc/server";
 import { AgentsInsertSchema } from "../schema";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
     getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
         const [existingAgent] = await db
-            .select()
+            .select({
+                ...getTableColumns(agents),
+                meetingCount: sql<number>`5`
+            })
             .from(agents)
             .where(eq(agents.id, input.id))
 
@@ -18,7 +21,10 @@ export const agentsRouter = createTRPCRouter({
     }),
     getMany: protectedProcedure.query(async () => {
         const data = await db
-            .select()
+            .select({
+                ...getTableColumns(agents),
+                meetingCount: sql<number>`5`
+            })
             .from(agents);
 
         //throw new TRPCError({ code: "BAD_REQUEST" })
