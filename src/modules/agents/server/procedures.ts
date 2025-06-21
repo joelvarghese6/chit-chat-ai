12332d890cfe43a14779819ec094cef9a58a9e6f
Joel <varghesejoel6@gmail.com>
2025-06-21 11:24:01 +0530

    agents filter

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -4,7 +4,9 @@ import { agents } from "@/db/schema"
 import { TRPCError } from "@trpc/server";
 import { AgentsInsertSchema } from "../schema";
 import { z } from "zod";
-import { eq, getTableColumns, sql } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
+import { Search } from "lucide-react";
 
 export const agentsRouter = createTRPCRouter({
     getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
@@ -19,17 +21,53 @@ export const agentsRouter = createTRPCRouter({
         //throw new TRPCError({ code: "BAD_REQUEST" })
         return existingAgent;
     }),
-    getMany: protectedProcedure.query(async () => {
-        const data = await db
-            .select({
-                ...getTableColumns(agents),
-                meetingCount: sql<number>`5`
-            })
-            .from(agents);
+    getMany: protectedProcedure
+        .input(z.object({
+            page: z.number().default(DEFAULT_PAGE),
+            pageSize: z.number()
+                .min(MIN_PAGE_SIZE)
+                .max(MAX_PAGE_SIZE)
+                .default(DEFAULT_PAGE_SIZE),
+            search: z.string().nullish()
+        })
+        )
+        .query(async ({ ctx, input }) => {
+            const { search, page, pageSize } = input;
 
-        //throw new TRPCError({ code: "BAD_REQUEST" })
-        return data;
-    }),
+            const data = await db
+                .select({
+                    ...getTableColumns(agents),
+                    meetingCount: sql<number>`5`
+                })
+                .from(agents)
+                .where(
+                    and(
+                        eq(agents.userId, ctx.auth.user.id),
+                        search ? ilike(agents.name, `%${search}%`) : undefined
+                    )
+                )
+                .orderBy(desc(agents.createdAt), desc(agents.id))
+                .limit(pageSize)
+                .offset((page - 1) * pageSize)
+
+            const [total] = await db
+                .select({ count: count() })
+                .from(agents)
+                .where(
+                    and(
+                        eq(agents.userId, ctx.auth.user.id),
+                        search ? ilike(agents.name, `%${search}%`) : undefined
+                    )
+                )
+            const totalPages = Math.ceil(total.count / pageSize)
+
+            //throw new TRPCError({ code: "BAD_REQUEST" })
+            return {
+                items: data,
+                total: total.count,
+                totalPages
+            };
+        }),
     create: protectedProcedure
         .input(AgentsInsertSchema)
         .mutation(async ({ input, ctx }) => {
